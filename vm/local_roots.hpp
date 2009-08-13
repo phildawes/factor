db1341ed544f2231cbf357588a0a890277065707
Phil Dawes <phil@phildawes.net>
2009-08-13 15:01:34 +0100

    moved global state from math into vm

--- vm/local_roots.hpp
+++ vm/local_roots.hpp
@@ -1,14 +1,3 @@
 namespace factor
 {
-
-/* If a runtime function needs to call another function which potentially
-allocates memory, it must wrap any local variable references to Factor
-objects in gc_root instances */
-extern segment *gc_locals_region;
-extern cell gc_locals;
-
-/* A similar hack for the bignum implementation */
-extern segment *gc_bignums_region;
-extern cell gc_bignums;
-
 }
